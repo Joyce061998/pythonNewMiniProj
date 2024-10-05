60924b2f92a6b09ee258da4a209ba2ead91133e9
Jamela <mejiajoyceai98@gmail.com>
2024-10-05 11:01:20 +0800

    Mama

--- venv/Guess Random Game.py
+++ venv/Guess Random Game.py
@@ -4,15 +4,20 @@ Pseudocode
 2. each guess has 5 limit, if the player is not guessing the right game plus 1 bet and it will limit the play up to 5
 3. The game will ask if you wanna play or not if yes (the player will bet upto 5 limit), if no it will terminate the game
 
-
-
 """
-print(" 😆 ❤ Welcome to Guessing Number Game ❤ 😆")
-print("               🥰🥰🥰                                ")
+welcome = """
+          😆 ❤ Welcome to Guessing Number Game ❤ 😆
+                            🥰🥰🥰 
+    Instrution : You need to know the random number from 1-100. 
+    You need to guess the number until you reach the 5 chances.
+                        Good Luck!!!
+"""
+print(welcome)
 
-player = input("Do you wanna play: Y/N: ")
+
+player = input("Do you wanna play: Select Y/N: ")
 if player.upper() == "Y" or player.lower() == "y":
-    secret_number = 9
+    secret_number =  100
     i = 0
     while i < 5:
         guess = int(input("Guess Number From 1-100: "))
@@ -26,4 +31,9 @@ elif player.lower() == "n" or player.upper() == "N":
     print(" 🫡 Play Next Time! 🫡")
 else:
     print("I dont understand")
+blntime = """        
+        Better  Luck Next Time!!!
+        You can Bet Again By Simple Run the Site
+"""
+print(blntime)
 
