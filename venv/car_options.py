31d70a380a52b6250d3d3104ca7ccf9674dc0eb8
Jamela <mejiajoyceai98@gmail.com>
2024-10-05 11:04:04 +0800

    Mama

---
+++ venv/car_options.py
@@ -0,0 +1,30 @@
+#other solution
+welcome1= """
+Welcome to Car Option 
+"""
+print(welcome1)
+help=input(">")
+help1 = """
+start -  to start the car - please select 1
+stop- to stop the car - please select 2
+quit- to exit - please select 3
+"""
+print(help1)
+car_count = 0
+car_limit = 3
+
+while car_count < car_limit:
+    car = int(input("Please select the Option: "))
+    car_count += 1
+    if car == 1:
+     print("Start Engine!")
+     continue
+    elif car == 2:
+        print("Stop Engine")
+        continue
+    elif car == 3:
+        print("Bye")
+        break
+    else:
+     print("I Dont Understand")
+
