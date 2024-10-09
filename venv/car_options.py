8f842382975f95d47c2d1201dcfcf15ab31807a8
Jamela <mejiajoyceai98@gmail.com>
2024-10-09 11:04:18 +0800

    Python Update

--- venv/car_options.py
+++ venv/car_options.py
@@ -3,16 +3,21 @@ welcome1= """
 Welcome to Car Option 
 """
 print(welcome1)
-help=input(">")
-help1 = """
-start -  to start the car - please select 1
-stop- to stop the car - please select 2
-quit- to exit - please select 3
-"""
-print(help1)
+help=input("Type Help>")
+
+title = str("Help")
+if help == title:
+    help1 = """
+    start -  to start the car - please select 1
+    stop- to stop the car - please select 2
+    quit- to exit - please select 3
+    """
+    print(help1)
+else:
+    help = input(">")
+
 car_count = 0
 car_limit = 3
-
 while car_count < car_limit:
     try:
         car = int(input("Please select the Option: "))
@@ -25,7 +30,6 @@ while car_count < car_limit:
             continue
         elif car == 3:
             print("Bye")
-            break
         else:
             print("I Dont Understand")
     except:
