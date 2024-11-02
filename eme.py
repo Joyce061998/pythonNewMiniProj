69b0e4cd37072619973200d42a6bc85e81a890ef
Jamela <mejiajoyceai98@gmail.com>
2024-11-02 22:08:59 +0800

    Learn Mama Mo

--- eme.py
+++ eme.py
@@ -1,5 +1,15 @@
-for x in 'banana':
+for x in 'banana ':
     print(x)
 '''
 it mean you just looping the valur of x which is Banana
+'''
+length_of_letter = "Hello world"
+print(len(length_of_letter))
+
+color = ['red', 'blue', 'yellow']
+color.append('green')
+print(color)
+
+'''
+append means add
 '''
